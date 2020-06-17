45a4db5c56dd2ed9b67019c1f5c1ca1316ff046c
tangoyankee <miller.tim108@gmail.com>
2020-06-16 17:01:55 -0700

    remove cars server side db

--- test.js
+++ test.js
@@ -7,3 +7,31 @@ db.info()
 .catch(err => {
     console.warn(err)
 })
+
+
+const username = env.COUCHDB_USERNAME || 'firstObserver'
+const password = env.COUCHDB_PASSWORD || 'firstObserverPassword'
+const domain = env.COUCHDB_DOMAIN || 'localhost'
+const port = env.COUCHDB_PORT || 5984
+
+let headers = new Headers()
+headers.set('Content-Type', 'application/json')
+// headers.set('Authorization', 'Basic ' + btoa(username + ":" + password))
+
+const authData = {
+    name: username,
+    password: password
+}
+
+const options = {
+    method: 'POST',
+    headers: headers,
+    mode: 'cors',
+    credentials: 'include',
+    body: JSON.stringify(authData)
+}
+fetch(`http://${domain}:${port}/_session`, options)
+    .then(response => {
+        console.log(response)
+    })
+    .catch(err => console.warn('Error', err))
