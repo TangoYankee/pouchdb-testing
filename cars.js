bf26f6eada250c0d111adbe501f3268c181ed0c0
tangoyankee <miller.tim108@gmail.com>
2020-06-18 17:20:10 -0700

    notes for production

--- cars.js
+++ cars.js
@@ -3,14 +3,17 @@ const password = env.COUCHDB_PASSWORD || 'firstObserverPassword'
 const domain = env.COUCHDB_DOMAIN || 'localhost'
 const port = 3000
 
+// TODO: Prod/Dev protocol needs to changed between http and https
 const carsDBR = new PouchDB(`http://${domain}:${port}/cars`,
     {
+        // Whether to include basic authorization changes depending on whether there is an origin
         auth:
         {
             "username": username,
             "password": password
         },
         fetch: (url, opts) => {
+            // Whether to include credentials changes when there is an origin
             opts.credentials = 'omit'
             return PouchDB.fetch(url, opts)
         }
